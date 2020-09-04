d895f2735f920d8bc937d34fae3654a40c56f50f
Jordan_Torres <jt.arontorres@gmail.com>
2020-09-03 20:23:41 -0700

    second commit

--- calculator.py
+++ calculator.py
@@ -5,4 +5,13 @@ number = input("Pick a number")
 #question asking how many eggs a small child can hold
 othernumber = input("pick another number ")
 
-print(f"your new number is {int(number) + int(othernumber)}.")
+new_number = input("Would you like to add, subtract, or multiply them? ")
+
+if new_number == "add":
+    print(f"You picked add, your new number is {int(number) + int(othernumber)}.")
+
+elif new_number == "subtract":
+    print(f"You picked subtract, your new number is {int(number) - int(othernumber)}.")
+
+elif new_number == "multiply":
+    print(f"You picked multiply, your new number is {int(number) * int(othernumber)}.")
